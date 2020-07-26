b1368842ee6bbb71530fe2e4351bd069c73def39
Mike E <kitkat@hey.com>
2020-07-26 13:21:44 -0400

    Add style changes

--- index.js
+++ index.js
@@ -1,64 +1,64 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 const moment = require('moment')
-const fetch = require('node-fetch');
-const { CronJob } = require('cron');
+const fetch = require('node-fetch')
+const { CronJob } = require('cron')
 const urlencode = require('urlencode')
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
+// eslint-disable-next-line no-unused-vars
 const cron = require('cron').CronJob
-const client = new Discord.Client();
+const client = new Discord.Client()
 const discordToken = process.env.DISCORD_BOT_TOKEN
 const twitchToken = process.env.TWITCH_OAUTH_TOKEN
 
 var bobbyChannel
 var simpChannel
-var cmdPrefix = "!p";
+var cmdPrefix = '!p'
 var alertSent = false
 
-function calcBobbyTime() {
+function calcBobbyTime () {
   const releaseDate = moment('2020.12.11', 'YYYY.MM.DD')
   const rn = moment()
   return releaseDate.diff(rn, 'days')
 }
 
-function calcRowdyTime() {
+function calcRowdyTime () {
   const rReleaseDate = moment('2020.12.15', 'YYYY.MM.DD')
   const rn = moment()
   return rReleaseDate.diff(rn, 'days')
 }
 
-
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`)
   console.log('started bobby cronjob')
   simpChannel = client.channels.cache.get('690014059663458310')
   bobbyChannel = client.channels.cache.get('736706894667841626')
 
-  var freeBobbyMessage = new CronJob('0 0 * * *', function(){
+  var freeBobbyMessage = new CronJob('0 0 * * *', function () {
     bobbyChannel.send(`Bobby will be free in ${calcBobbyTime()} days!`)
     console.log('fired bobby message')
   }, null, true)
 
   freeBobbyMessage.start()
-  setInterval(function() {
-    pokiStreamUrl = 'https://api.twitch.tv/kraken/streams/44445592'
+  setInterval(function () {
+    const pokiStreamUrl = 'https://api.twitch.tv/kraken/streams/44445592'
     fetch(pokiStreamUrl, {
-        headers: {
-            'Accept': 'application/vnd.twitchtv.v5+json',
-            'Client-ID': 'zfwmhzfmpxrhdoufl2cwf7z2iqlk6l',
-            'Authorization': `Bearer ${twitchToken}`,
-        },
-        method: 'GET',
+      headers: {
+        Accept: 'application/vnd.twitchtv.v5+json',
+        'Client-ID': 'zfwmhzfmpxrhdoufl2cwf7z2iqlk6l',
+        Authorization: `Bearer ${twitchToken}`
+      },
+      method: 'GET'
     })
-    .then(response => response.json())
-    .then(data => {
+      .then(response => response.json())
+      .then(data => {
         if (data.stream == null) {
-          client.user.setActivity('Not Pokimane', { type: 'WATCHING' });
-          alertSent = false          
+          client.user.setActivity('Not Pokimane', { type: 'WATCHING' })
+          alertSent = false
         } else {
-          client.user.setActivity('Pokimane', { type: 'WATCHING' });
-          if (alertSent == false) {
-            liveEmbed = new Discord.MessageEmbed()
+          client.user.setActivity('Pokimane', { type: 'WATCHING' })
+          if (alertSent === false) {
+            var liveEmbed = new Discord.MessageEmbed()
             liveEmbed.setColor('#eb3327')
             liveEmbed.setTitle('@here, Pokimane is Live on Twitch!')
             liveEmbed.setImage(data.stream.preview.large)
@@ -70,11 +70,11 @@ https://twitch.tv/pokimane`)
             alertSent = true
           }
         }
-    })
-  }, 5000);
-});
+      })
+  }, 5000)
+})
 
-function dpSearch(type, query) {
+function dpSearch (type, query) {
   var baseSearchUrl = 'https://www.dogpile.com/search/'
   if (type === 'web') {
     baseSearchUrl = baseSearchUrl + 'web?q='
@@ -88,14 +88,14 @@ function dpSearch(type, query) {
   if (type === 'image') {
     return fetch(searchUrl, {
       headers: {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15'
       }
     }).then((res) => {
       return res.text()
     }).then((data) => {
       const dom = new JSDOM(data)
       var results = dom.window.document.getElementsByClassName('link')
-      if (results.length == 0) {
+      if (results.length === 0) {
         return 'No results.'
       } else {
         return results[0].href
@@ -104,14 +104,14 @@ function dpSearch(type, query) {
   } else {
     return fetch(searchUrl, {
       headers: {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15'
       }
     }).then((res) => {
       return res.text()
     }).then((data) => {
       const dom = new JSDOM(data)
       var results = dom.window.document.getElementsByClassName('web-bing__url')
-      if (results.length == 0) {
+      if (results.length === 0) {
         return 'No results.'
       } else {
         return results[0].innerHTML
@@ -123,31 +123,29 @@ function dpSearch(type, query) {
 // essentially bot commands
 client.on('message', msg => {
   // Simple say command
-  var msgArray = msg.content.split(" ");
-  var prefix = "";
-  var command = "";
+  var msgArray = msg.content.split(' ')
+  var prefix = ''
+  var command = ''
 
   // Get the prefix and the command
   if (msgArray.length >= 2) {
-    prefix = msgArray.shift();
-    command = msgArray.shift();
+    prefix = msgArray.shift()
+    command = msgArray.shift()
   }
 
   // help
   if (msgArray.join(' ') === '!help') {
     msg.author.send('read the code: https://github.com/AndroidKitKat/pokibot')
-
   }
 
-  //dogpile search
+  // dogpile search
   if (prefix === '!g') {
     const duckQuery = command + ' ' + msgArray.join(' ')
     dpSearch('web', duckQuery).then((searchResult) => {
       msg.channel.send(searchResult)
     })
-    
   }
-  //dogpile image search
+  // dogpile image search
   if (prefix === '!gis') {
     const imageQuery = command + ' ' + msgArray.join(' ')
     dpSearch('image', imageQuery).then((searchResult) => {
@@ -156,23 +154,23 @@ client.on('message', msg => {
   }
   // free bobby!
   if (prefix === '!free') {
-   if (command === 'bobby') {
-     bobbyEmbed = new Discord.MessageEmbed()
-     bobbyEmbed.setColor('#eb3327')
-     bobbyEmbed.setTitle('Pokimane?')
-     bobbyEmbed.setDescription('Nah, pokemon')
-     bobbyEmbed.setImage('https://www.mypokecard.com/en/Gallery/my/galery/AUFz107M7SKK.jpg')
-     msg.channel.send(bobbyEmbed)
-     // Calculate time till bobby is free
-     msg.channel.send(`Bobby will be free in ${calcBobbyTime()} days!`)
-   } else if (command === 'rowdy') {
-     msg.channel.send(`Rowdy will be free in ${calcRowdyTime()} days!`)
-   }
+    if (command === 'bobby') {
+      var bobbyEmbed = new Discord.MessageEmbed()
+      bobbyEmbed.setColor('#eb3327')
+      bobbyEmbed.setTitle('Pokimane?')
+      bobbyEmbed.setDescription('Nah, pokemon')
+      bobbyEmbed.setImage('https://www.mypokecard.com/en/Gallery/my/galery/AUFz107M7SKK.jpg')
+      msg.channel.send(bobbyEmbed)
+      // Calculate time till bobby is free
+      msg.channel.send(`Bobby will be free in ${calcBobbyTime()} days!`)
+    } else if (command === 'rowdy') {
+      msg.channel.send(`Rowdy will be free in ${calcRowdyTime()} days!`)
+    }
   }
   // homo simpians
   if (prefix === '!simp') {
     if (command === 'alert') {
-      simpEmbed = new Discord.MessageEmbed()
+      var simpEmbed = new Discord.MessageEmbed()
       simpEmbed.setColor('#b970df')
       simpEmbed.setTitle('Hey man')
       simpEmbed.setDescription('We only simp ironically here')
@@ -182,19 +180,17 @@ client.on('message', msg => {
   }
 
   if (prefix === cmdPrefix) { // Only look at messages with the correct command prefix
-    if (command === "hello") {
-      msg.channel.send("hello");
-    }
-    else if (command === "say") {
+    if (command === 'hello') {
+      msg.channel.send('hello')
+    } else if (command === 'say') {
       if (msgArray.length >= 1) {
-        msg.channel.send(msgArray.join(" "));
+        msg.channel.send(msgArray.join(' '))
+      } else {
+        msg.channel.send('Hey you fucked up, give me something to say, !p say <message>')
       }
-      else {
-        msg.channel.send("Hey you fucked up, give me something to say, !p say <message>");
-      }
-    } else if (command === "sub") {
+    } else if (command === 'sub') {
       // check for the sub tiers
-      if (msgArray.join(" ").toLowerCase() === 'tier 3') {
+      if (msgArray.join(' ').toLowerCase() === 'tier 3') {
         const t3embed = new Discord.MessageEmbed()
         console.log(msg)
         t3embed.setTitle('Thanks for the Sub ^_^')
@@ -202,23 +198,23 @@ client.on('message', msg => {
         t3embed.setDescription(`I reaaallllyyy appreciate the sub <@${msg.author.id}>.`)
         t3embed.setImage('https://media.tenor.com/images/0de2b320fc290bd68a63f55431f9bf4f/tenor.gif')
         msg.channel.send(t3embed)
-      } else if (msgArray.join(" ").toLowerCase() === 'tier 2') {
+      } else if (msgArray.join(' ').toLowerCase() === 'tier 2') {
         const t2embed = new Discord.MessageEmbed()
         t2embed.setTitle('Thanks for the sub')
         t2embed.setColor('#b970df')
-        t2embed.setDescription(`Thanks, but it won't buy me a new computer`)
+        t2embed.setDescription('Thanks, but it won\'t buy me a new computer')
         t2embed.setImage('https://thumbs.gfycat.com/BlissfulBriefAngwantibo-max-1mb.gif')
         msg.channel.send(t2embed)
-      } else if (msgArray.join(" ").toLowerCase() === 'tier 1') {
+      } else if (msgArray.join(' ').toLowerCase() === 'tier 1') {
         const t1embed = new Discord.MessageEmbed()
         t1embed.setTitle('...')
         t1embed.setColor('#b970df')
-        t1embed.setDescription(`Thanks, I guess...`)
+        t1embed.setDescription('Thanks, I guess...')
         t1embed.setImage('https://www.talkesport.com/wp-content/uploads/tenor-1.gif')
         msg.channel.send(t1embed)
       }
-    } else if (command === "pokigasm") {
-      gasmEmbed = new Discord.MessageEmbed()
+    } else if (command === 'pokigasm') {
+      var gasmEmbed = new Discord.MessageEmbed()
       gasmEmbed.setColor('#b970df')
       gasmEmbed.setImage('https://thumbs.gfycat.com/DimFancyFairyfly-size_restricted.gif')
       gasmEmbed.setTitle('Pokigasm')
@@ -226,7 +222,7 @@ client.on('message', msg => {
       msg.channel.send(gasmEmbed)
     } else if (command === 'send') {
       if (msgArray.join(' ') === 'feet pics maybe?') {
-        feetEmbed = new Discord.MessageEmbed()
+        var feetEmbed = new Discord.MessageEmbed()
         feetEmbed.setColor('#b970df')
         feetEmbed.setTitle('Well...')
         feetEmbed.setDescription('You did donate soooo....')
@@ -235,8 +231,6 @@ client.on('message', msg => {
       }
     }
   }
-});
+})
 
-
-
-client.login(discordToken);
+client.login(discordToken)
