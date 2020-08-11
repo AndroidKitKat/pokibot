208c1b279159c0e259009341814fa8fe6250b97a
マイケル・アイズマン <japanese_kitkat@yahoo.co.jp>
2020-08-11 14:44:44 -0400

    fix gis

--- modules/gis.js
+++ modules/gis.js
@@ -20,19 +20,13 @@ module.exports = {
 }
 
 function bingSearch(query) {
-
-var url = 'https://api.cognitive.microsoft.com/bing/v7.0/images/search?q='
-const bingKey = process.env.BING_KEY_ONE
-var searchTerm = urlencode(query)
-var bingHeaders = {
-  'Ocp-Apim-Subscription-Key': bingKey,
-}
-
-fetch(url + searchTerm, {
-  headers: bingHeaders
-}).then(res => {
-  return res.json()
-}).then(data => {
-  return data.value[Math.floor(Math.random() * data.value.length)].contentUrl
-})
+  return fetch('https://api.cognitive.microsoft.com/bing/v7.0/images/search?q=' + urlencode(query), {
+    headers: {
+      'Ocp-Apim-Subscription-Key': process.env.BING_KEY_ONE,
+    }
+  }).then(res => {
+    return res.json()
+  }).then(data => {
+    return data.value[Math.floor(Math.random() * data.value.length)].contentUrl
+  })
 }
